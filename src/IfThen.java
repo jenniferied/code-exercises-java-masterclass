caf94983c65e566e7e8b2566c90689ee7907aba2
Jennifer Meier <m.jennifer@outlook.com>
2021-05-27 03:34:46 +0200

    Update IfThen & Possible Corrections For Project Structure Issues

--- src/IfThen.java
+++ src/IfThen.java
@@ -1,2 +1,25 @@
 public class IfThen {
+    public static void main(String[] args) {
+        ifAndThen();
+        breakLine();
+        ifOrThen();
+    }
+
+    static void breakLine() {
+        System.out.println();
+    }
+
+    static void ifAndThen() {
+        int intValue = 10;
+        if(12 > intValue && intValue < 15) {
+            System.out.println("Both expressions evaluate to true.");
+        }
+    }
+
+    static void ifOrThen() {
+        int intValue = 10;
+        if(12 > intValue || intValue < 11) {
+            System.out.println("One of the expressions evaluates to true.");
+        }
+    }
 }
