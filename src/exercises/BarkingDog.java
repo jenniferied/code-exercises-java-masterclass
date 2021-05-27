e09b25eec289b79f06ab68a190acff107f660eb6
Jennifer Meier <m.jennifer@outlook.com>
2021-05-27 20:58:53 +0200

    Add completed BarkingDog

---
+++ src/exercises/BarkingDog.java
@@ -0,0 +1,24 @@
+/* Code Exercise 03
+* We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
+* Write a method shouldWakeUp that has 2 parameters.
+* 1st parameter should be of type boolean and be named barking it represents if our dog is currently barking.
+* 2nd parameter represents the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23.
+* We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true.
+* In all other cases return false.
+*
+ */
+
+package exercises;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 23));
+        System.out.println(shouldWakeUp(true, 6));
+        System.out.println(shouldWakeUp(false, 23));
+        System.out.println(shouldWakeUp(true, 10));
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        return barking && (hourOfDay < 8 || hourOfDay > 22) && hourOfDay >= 0 && hourOfDay <= 23;
+    }
+}
