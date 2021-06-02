1960136d96ed8b344b59fe4bf93cc4429536b814
Jennifer Meier <m.jennifer@outlook.com>
2021-06-02 14:47:03 +0200

    Edit main to add missing }

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -3,9 +3,10 @@ package inheritance;
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("Animal", 1, 1, 5, 5);
-        Dog dog = new Dog("Yorkie", 2, 4, 2, 4, 1, 20,"silky");
+        Dog dog = new Dog("Yorkie", 2, 4, 2, 4, 1, 20, "silky");
         dog.eat();
         dog.move(0);
         dog.walk();
         dog.run();
     }
+}
